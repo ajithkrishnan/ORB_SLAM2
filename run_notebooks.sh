91c45e80f722dcd3f499e0a648cbba47230f7c22
Ajith Krishnan <ajithkrishnanbm@gmail.com>
2020-01-29 15:19:19 +0000

    added allow root option

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -2,4 +2,4 @@
 
 port=4333
 
-. /share/projects/2019_kss_persistent_jupyter_on_gpu1/ws/devel/setup.bash ; pwd; ls;  jupyter notebook --NotebookApp.token='al_token' --notebook-dir=/share/projects/2019_kss_personal_jupyter_notebooks/trn_ak/ --ip 0.0.0.0 --port $port 
+. /share/projects/2019_kss_persistent_jupyter_on_gpu1/ws/devel/setup.bash ; pwd; ls;  jupyter notebook --NotebookApp.token='al_token' --notebook-dir=/share/projects/2019_kss_personal_jupyter_notebooks/trn_ak/ --ip 0.0.0.0 --port $port --allow-root
