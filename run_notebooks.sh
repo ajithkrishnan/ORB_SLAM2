c793561a5ae00fe114138f34b7c916110d4bdfcf
Ajith Krishnan <ajithkrishnanbm@gmail.com>
2020-01-29 15:03:34 +0000

    added script for jupyter

---
+++ run_notebooks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+port=4333
+
+. /share/projects/2019_kss_persistent_jupyter_on_gpu1/ws/devel/setup.bash ; pwd; ls;  jupyter notebook --NotebookApp.token='al_token' --notebook-dir=/share/projects/2019_kss_personal_jupyter_notebooks/trn_ak/ --ip 0.0.0.0 --port $port 
