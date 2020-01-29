2e0773c64f2508b766322c7f7b9c6d2404ca30a0
Ajith Krishnan <ajithkrishnanbm@gmail.com>
2020-01-29 15:36:41 +0000

    fixed browser bug

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-port=4333
+#port=4333
+port=8888
 
 . /share/projects/2019_kss_persistent_jupyter_on_gpu1/ws/devel/setup.bash ; pwd; ls;  jupyter notebook --NotebookApp.token='al_token' --notebook-dir=/share/projects/2019_kss_personal_jupyter_notebooks/trn_ak/ --ip 0.0.0.0 --port $port --allow-root
