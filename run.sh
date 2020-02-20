77836c1864bffa0482d6ae0c4234fa467c0b9850
Ajith Krishnan <ajithkrishnanbm@gmail.com>
2020-02-20 10:28:33 +0000

    added path to eval bags

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#DATA_PATH="/share/projects/2019_kss_personal_jupyter_notebooks/trn_ak/sts_odom_dataset/paketzentrum_eifeltor/sequences/"
-DATA_PATH="/share/projects/2019_kss_personal_jupyter_notebooks/trn_ak/sts_odom_dataset/paketzentrum_eifeltor/sequences/2019-05-15-14-55-45_hmm"
+#DATA_PATH="/share/projects/2019_kss_personal_jupyter_notebooks/trn_ak/sts_odom_dataset/paketzentrum_eifeltor/sequences/2019-05-15-14-55-45_hmm"
+DATA_PATH="/share/projects/2019_kss_personal_jupyter_notebooks/trn_ak/sts_odom_dataset/paketzentrum_eifeltor/eval_bags/*"
 
 ./Examples/Monocular/mono_kitti Vocabulary/ORBvoc.txt Examples/Monocular/sts.yaml $DATA_PATH
 
